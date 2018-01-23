e62fb3ecd34fd9fa131a72d40a8b1cb78722650f
Studium <Studium@DESKTOP-9OS1COE>
2018-01-23 11:46:34 +0100

    Primer commit

---
+++ MS/src/es/studium/Mayorsuma/Mayorsuma.java
@@ -0,0 +1,31 @@
+package es.studium.Mayorsuma;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Mayorsuma
+{
+
+	public static void main(String[] args)throws NumberFormatException, IOException
+	{
+		int numero1, numero2, numero3, suma;
+		BufferedReader lectura = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Dame el número 1");
+		numero1=Integer.parseInt(lectura.readLine());
+		System.out.println("Dame el número 2");
+		numero2=Integer.parseInt(lectura.readLine());
+		suma=numero1+numero2;
+		do
+		{
+			System.out.println("Dame el número 3:");
+			numero3=Integer.parseInt(lectura.readLine());
+		if(suma>=numero3)
+		{
+				suma=numero2+numero3;
+				numero2=numero3;
+		}
+		}
+		while(suma>=numero3);
+	}
+}
